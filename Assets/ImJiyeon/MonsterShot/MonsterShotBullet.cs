fb94aa78c40b93108703de1e7933023ef1b1c020
jiyeonUning <yeon14735@gmail.com>
2024-10-28 16:25:36 +0900

    [fix] 플레이어 체력, 몬스터 데미지 수치 등 PlayerDataModel 연동
    임시적으로 지정해둔 각 값들을 플레이어의 Model 수치와 연동하여 플레이어와의 상호작용을 활성화 하였음

--- Assets/ImJiyeon/MonsterShot/MonsterShotBullet.cs
+++ Assets/ImJiyeon/MonsterShot/MonsterShotBullet.cs
@@ -1,19 +1,20 @@
 using UnityEngine;
-using UnityEngine.Pool;
 
 public class MonsterShotBullet : MonoBehaviour
 {
-    //[SerializeField] GameObject target;
     [SerializeField] GameObject Player;
-            private Rigidbody2D rb;
+    [SerializeField] PlayerDataModel playerDataModel;
+             private Rigidbody2D rb;
 
     [SerializeField] float monsterAttackSpeed;
+    [SerializeField] int   monsterAttack;
     [SerializeField] float returnTime;
              private float remainTime;
 
     private void Awake()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
+        playerDataModel = Player.GetComponent<PlayerDataModel>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -24,23 +25,20 @@ public class MonsterShotBullet : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         // 플레이어의 콜라이더와 몬스터의 총알이 맞닿았을 때, 해당 총알은 삭제된다.
-        if (Player == collision.gameObject) { Destroy(gameObject); }
-
-        // + 플레이어 체력에 MonsterAttack 만큼 -- 해주는 코드를 구성할 것
+        if (Player == collision.gameObject)
+        {
+            playerDataModel.Health -= monsterAttack;
+            Destroy(gameObject);
+        }
     }
 
     void Update()
     {
+        // 총알은 생성 시 자동으로 플레이어를 향해 발사된다.
         transform.Translate(Player.transform.position * monsterAttackSpeed * Time.deltaTime);
 
-        // 총알이 맞지 않고 일정 시간이 지났을 때도 자동으로 삭제된다.
+        // 총알이 맞지 않고 일정 시간이 지났을 때, 자동으로 삭제된다.
         remainTime -= Time.deltaTime;
         if (remainTime < 0) { Destroy(gameObject); }
     }
-
-    //public void SetTarget(GameObject target)
-    //{
-    //    this.target = target;
-    //}
-
 }
