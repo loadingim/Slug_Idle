36e31037180ab0a5273b4d5de1ed4f0faf0e0815
tjdeo1102 <57614322+tjdeo1102@users.noreply.github.com>
2024-10-28 20:05:51 +0900

    [Fix] 아이템 슬롯 스왑 기능 보완
    - 기존에 아이템끼리 스왑이 안되는 경우를 해결

--- Assets/KwakSeongDae/Scripts/InventoryItem.cs
+++ Assets/KwakSeongDae/Scripts/InventoryItem.cs
@@ -5,9 +5,10 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 /// <summary>
-/// 인벤토리 아이템 (무기, 스킬 등)은 해당 스크립트 포함 필요,
+/// 인벤토리 아이템 (무기, 스킬 등)은 실제 아이템과 독립적
 /// 무기나 스킬 스크립트를 포함한 
 /// </summary>
+/// <param name="ItemObject"> 실제 아이템에 대한 참조 (슬롯의 타입에 따라, 아이템 활성화 여부 결정) </param>
 public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     // 슬롯의 종류(현재 장착 슬롯, 보관용 슬롯)에 따라 해당 게임 오브젝트 활성화 여부 체크
@@ -15,12 +16,17 @@ public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     [HideInInspector] public Transform parentAfterDrag; // 드래그 후에 다시 원래 위치로 돌아갈 부모 트랜스폼
 
     private Image image;
+    // 이미지UI가 없는 경우에 비정상적인 인벤토리 아이템이라 간주하고 비활성화
     void OnEnable()
     {
         if (TryGetComponent<Image>(out image) == false)
         {
-            // UI에서 사용될 이미지
-            image = gameObject.AddComponent<Image>();
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            // 현재 자신의 슬롯 체크
+            SlotCheck();
         }
     }
 
@@ -42,7 +48,25 @@ public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     {
         image.raycastTarget = true;
         transform.SetParent(parentAfterDrag);
+        SlotCheck();
     }
 
-
+    /// <summary>
+    /// 현재 자신이 속한 슬롯의 타입을 체크하여 실제 아이템을 컨트롤하는 함수
+    /// </summary>
+    public void SlotCheck()
+    {
+        if (transform.parent.TryGetComponent<InventorySlot>(out var slot))
+        {
+            switch (slot.slotType)
+            {
+                case InventorySlot.SlotType.ActiveSlot:
+                    ItemObject.SetActive(true);
+                    break;
+                case InventorySlot.SlotType.StorageSlot:
+                    ItemObject.SetActive(false);
+                    break;
+            }
+        }
+    }
 }
--- Assets/KwakSeongDae/Scripts/InventorySlot.cs
+++ Assets/KwakSeongDae/Scripts/InventorySlot.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-
-
 public class InventorySlot : MonoBehaviour, IDropHandler
 {
     public enum SlotType
@@ -34,8 +32,14 @@ public class InventorySlot : MonoBehaviour, IDropHandler
                 //원래 돌아가려고 했던 트랜스폼에 현재 자신이 가진 아이템을 넣기
                 var swapObject = transform.GetChild(0);
                 swapObject.SetParent(Item.parentAfterDrag);
+                // 바뀐 후, 드래그 안되는 아이템에 대해서 슬롯 수동 체크
+                if (swapObject.TryGetComponent<InventoryItem>(out var Item2))
+                {
+                    Item2.SlotCheck();
+                }
+
                 //아이템이 들어갈곳을 parent로 설정
-                Item.parentAfterDrag = transform; 
+                Item.parentAfterDrag = transform;
             }
         }
     }
