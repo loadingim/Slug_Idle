e6cdfcbfa42c898a15f7c97cd0a6ec15e0e33692
Vivid <suook15@naver.com>
2024-10-28 16:56:38 +0900

    [Feat] 몬스터 Safe Zone 기능 추가
    1. Safe Zone 지나기 전 Monster Collider 비활성화
    2. Safe Zone 지난 후 Monster Collider 활성화

--- Assets/BaekSunmyung/Scripts/Stage.cs
+++ Assets/BaekSunmyung/Scripts/Stage.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
@@ -17,6 +18,9 @@ public class Stage : MonoBehaviour
     [SerializeField] private Transform monsterSpawnPoint;
     [SerializeField] private GameObject monsterPrefab;
 
+    [Header("Monster Safe Zone")]
+    [SerializeField] private Transform safeZone;
+
     [SerializeField] private int viewMonsterCount;
     [SerializeField] private int killMonsterCount;
     [SerializeField] private int totalMonsterCount;
@@ -27,22 +31,32 @@ public class Stage : MonoBehaviour
 
     private float killRate;
 
-    //임시 변수명
+    //Stage 관련 임시 변수명
     private MiddleMap curMiddleMap = MiddleMap.One;
     private int curSmallStage = 1;
     private int curWave = 1;
     private int maxWave = 4; // 추후 Data Table 에서 받아올 필요 있음
     private int curWaveMonsterCount = 5;
 
-    //현재 중분류 난이도 체크
+    //현재 중분류 난이도 체크 (임시 변수명)
     private Difficutly curDifficult = Difficutly.Easy;
     private bool[,] difficultCheck = new bool[(int)MiddleMap.SIZE, (int)Difficutly.SIZE];
 
+
+    //테스트 코드
+    [SerializeField] MonsterModel[] monsters;
+    int testIndex = 0;
+    //
+
     private void Start()
     {
+        //테스트 코드
+        monsters = new MonsterModel[curWaveMonsterCount];
+
+
+        //모든 중분류의 Easy 난이도는 True로 변경
         for (int i = 0; i < (int)MiddleMap.SIZE; i++)
         {
-            //ㅅ
             difficultCheck[i, 0] = true;
         }
 
@@ -67,6 +81,10 @@ public class Stage : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
 
+            monsters[testIndex].MonsterHP = 0;
+            Destroy(monsters[testIndex]);
+            testIndex++;
+
             viewMonsterCount--;
             killMonsterCount++;
 
@@ -74,14 +92,14 @@ public class Stage : MonoBehaviour
             killRate = ((float)killMonsterCount / totalMonsterCount) * 100f;
 
         }
-        else if (Input.GetKeyDown(KeyCode.Q))
-        {
-            viewMonsterCount++;
-        }
+
 
         //스테이지 진행률 UI 연동
         foreGround.fillAmount = killRate * 0.01f;
         StageClear();
+        MonsterSafeZone();
+
+
 
         if (viewMonsterCount < 1)
         {
@@ -103,10 +121,10 @@ public class Stage : MonoBehaviour
         }
         else
         {
-            
+
             ChangeDifficult();
-            
-            if((int)curMiddleMap < mapData.Count-1)
+
+            if ((int)curMiddleMap < mapData.Count - 1)
             {
                 curMiddleMap++;
             }
@@ -145,6 +163,10 @@ public class Stage : MonoBehaviour
         }
     }
 
+
+    /// <summary>
+    /// 현재 중분류 맵에서 난이도가 True 인 값을 찾아서 현재 난이도에 할당
+    /// </summary>
     public void SetDifficult()
     {
         //현재 중분류의 클리어한 난이도 확인
@@ -170,6 +192,9 @@ public class Stage : MonoBehaviour
 
     }
 
+    /// <summary>
+    /// Stage Wave 생성 
+    /// </summary>
     public void Wave()
     {
 
@@ -180,27 +205,86 @@ public class Stage : MonoBehaviour
         }
     }
 
-
+    /// <summary>
+    /// 몬스터 생성 기능
+    /// </summary>
     public void CreateMonster()
     {
+        testIndex = 0;
+
         //몬스터 클래스 받아와서 Instantiate 진행
         for (int i = 0; i < curWaveMonsterCount; i++)
         {
-
             //Offset 값은 기획 의도에 맞춰 변경
-            float xPos = UnityEngine.Random.Range(9.5f, 11f);
+            float xPos = UnityEngine.Random.Range(11f, 13f);
             float yPos = UnityEngine.Random.Range(2.5f, -3f);
             Vector3 offset = new Vector3(xPos, yPos, 0);
 
             GameObject monsterInstance = Instantiate(monsterPrefab, monsterSpawnPoint.position + offset, monsterSpawnPoint.rotation);
+
+            //몬스터 생성 시에는 Collider를 끔
+            Collider2D monsterCollider = monsterInstance.GetComponent<Collider2D>();
+            monsterCollider.enabled = false;
+
+            monsters[i] = monsterInstance.GetComponent<MonsterModel>();
+            AdjustmentStats(i);
+        }
+
+    }
+
+    /// <summary>
+    /// 몬스터 Collider On
+    /// </summary>
+    public void MonsterSafeZone()
+    {
+         
+        for (int i = 0; i < monsters.Length; i++)
+        {
+            //몬스터가 Null이 아닌 상태에서 SafeZone을 지났을 경우 Collider 활성화
+            if (monsters[i] != null)
+            {
+                if (monsters[i].transform.position.x < safeZone.transform.position.x)
+                {
+                    Collider2D collider = monsters[i].GetComponent<Collider2D>();
+                    collider.enabled = true;
+                }
+            }  
+        } 
+    }
+
+    /// <summary>
+    /// 난이도 별 스탯 조정
+    /// </summary>
+    /// <param name="index">몬스터 생성 시 받아올 인덱스</param>
+    public void AdjustmentStats(int index)
+    {
+        switch (curDifficult)
+        {
+            case Difficutly.Easy:
+                monsters[index].MonsterAttack = 3.5f;
+                break;
+
+            case Difficutly.Normal:
+                monsters[index].MonsterAttack = 5.5f;
+                break;
+
+            case Difficutly.Hard:
+                monsters[index].MonsterAttack = 7.5f;
+                break;
         }
 
     }
 
 
+    /// <summary>
+    /// 백그라운드 이미지 위치 리셋 액션
+    /// </summary>
+    /// <param name="action"></param>
     public void BackGroundResetAction(Action action)
     {
         bgAction = action;
     }
+
+
 
 }
