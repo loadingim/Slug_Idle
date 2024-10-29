f53202614590e81682df81295539aa63539bb66b
loadingim <68083530+loadingim@users.noreply.github.com>
2024-10-29 18:11:56 +0900

    [Feat] Weapon Create
    웨폰 구현 스크립트 생성

---
+++ Assets/LeeSangHak/Script/WeaponController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    //public List<Weapon>
+
+    
+/*
+    private void Update()
+    {
+        selectWeapon
+
+        if(selectWeapon)
+    }
+
+
+    public void switchWeapon(int x)
+    {
+        if ()
+    }*/
+
+
+}
