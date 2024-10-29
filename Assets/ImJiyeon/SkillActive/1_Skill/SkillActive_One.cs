92e30cae608979d6712453a22632fa2e2c070843
jiyeonUning <yeon14735@gmail.com>
2024-10-29 18:00:44 +0900

    [Feat] 첫번째 스킬 사용 기능 추가

--- Assets/ImJiyeon/SkillActive/1_Skill/SkillActive_One.cs
+++ Assets/ImJiyeon/SkillActive/1_Skill/SkillActive_One.cs
@@ -1,8 +1,23 @@
+using System.Collections;
 using UnityEngine;
 
 public class SkillActive_One : MonoBehaviour
 {
+    [SerializeField] bool isActived;
+    [SerializeField] int CoolTime;
 
 
+    public void SkillOne()
+    {
+        StartCoroutine(SkillOneActive());
+    }
 
+    IEnumerator SkillOneActive()
+    {
+        while (isActived)
+        {
+            Debug.Log("첫번째 스킬 사용됨");
+            yield return new WaitForSeconds(CoolTime);
+        }
+    }
 }
---
+++ Assets/ImJiyeon/SkillActive/SkillUseManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillUseManager : MonoBehaviour
+{
+    [SerializeField] Button ActiveOne;
+    [SerializeField] Button ActiveTwo;
+    [SerializeField] Button ActiveThr;
+    [SerializeField] Button ActiveFur;
+
+    private void Awake()
+    {
+        // 추후 스킬 사용 UI를 생성하며 비활성화로 돌릴 예정
+        ActiveOne.interactable = true;
+        ActiveTwo.interactable = true;
+        ActiveThr.interactable = true;
+        ActiveFur.interactable = true;
+    }
+}
