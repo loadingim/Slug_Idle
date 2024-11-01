fc50c438759bda514c3d5e1dbc1f15fae6d839f5
tjdeo1102 <57614322+tjdeo1102@users.noreply.github.com>
2024-11-01 20:23:07 +0900

    [Fix] 스탯 뷰어 불필요한 문구 수정

--- Assets/KwakSeongDae/Scripts/StatView.cs
+++ Assets/KwakSeongDae/Scripts/StatView.cs
@@ -41,33 +41,33 @@ public class StatView : MonoBehaviour
         switch (textView.stat)
         {
             case PlayerStatStoreData.Health:
-                textView.LevelText?.SetText($"{textView.prefixLevelText}: {PlayerDataModel.Instance.HealthLevel}");
-                textView.StatText?.SetText($"{textView.prefixStatText}: {PlayerDataModel.Instance.MaxHealth}"); //최대 체력을 표기
-                textView.BuyText?.SetText($"{textView.prefixBuyText}: {statStore.health.upValue}");
+                textView.LevelText?.SetText($"{textView.prefixLevelText} {PlayerDataModel.Instance.HealthLevel}");
+                textView.StatText?.SetText($"{textView.prefixStatText} {PlayerDataModel.Instance.MaxHealth}"); //최대 체력을 표기
+                textView.BuyText?.SetText($"{textView.prefixBuyText} {statStore.health.upValue}");
                 break;
 
             case PlayerStatStoreData.HealthRegen:
-                textView.LevelText?.SetText($"{textView.prefixLevelText}: {PlayerDataModel.Instance.HealthRegenLevel}");
-                textView.StatText?.SetText($"{textView.prefixStatText}: {PlayerDataModel.Instance.HealthRegen:F2}");
-                textView.BuyText?.SetText($"{textView.prefixBuyText}: {statStore.healthRegen.upValue}");
+                textView.LevelText?.SetText($"{textView.prefixLevelText} {PlayerDataModel.Instance.HealthRegenLevel}");
+                textView.StatText?.SetText($"{textView.prefixStatText} {PlayerDataModel.Instance.HealthRegen:F2}");
+                textView.BuyText?.SetText($"{textView.prefixBuyText} {statStore.healthRegen.upValue}");
                 break;
 
             case PlayerStatStoreData.Attack:
-                textView.LevelText?.SetText($"{textView.prefixLevelText}: {PlayerDataModel.Instance.AttackLevel}");
-                textView.StatText?.SetText($"{textView.prefixStatText}: {PlayerDataModel.Instance.Attack:F2}");
-                textView.BuyText.text = $"{textView.prefixBuyText}: {statStore.attack.upValue.ToString()}";
+                textView.LevelText?.SetText($"{textView.prefixLevelText} {PlayerDataModel.Instance.AttackLevel}");
+                textView.StatText?.SetText($"{textView.prefixStatText} {PlayerDataModel.Instance.Attack:F2}");
+                textView.BuyText.text = $"{textView.prefixBuyText} {statStore.attack.upValue.ToString()}";
                 break;
 
             case PlayerStatStoreData.TouchAttack:
-                textView.LevelText?.SetText($"{textView.prefixLevelText}: {PlayerDataModel.Instance.TouchAttackLevel}");
-                textView.StatText?.SetText($"{textView.prefixStatText}: {PlayerDataModel.Instance.TouchAttack:F2}");
-                textView.BuyText?.SetText($"{textView.prefixBuyText}: {statStore.touchAttack.upValue}");
+                textView.LevelText?.SetText($"{textView.prefixLevelText} {PlayerDataModel.Instance.TouchAttackLevel}");
+                textView.StatText?.SetText($"{textView.prefixStatText} {PlayerDataModel.Instance.TouchAttack:F2}");
+                textView.BuyText?.SetText($"{textView.prefixBuyText} {statStore.touchAttack.upValue}");
                 break;
 
             case PlayerStatStoreData.AttackSpeed:
-                textView.LevelText?.SetText($"{textView.prefixLevelText}: {PlayerDataModel.Instance.AttackSpeedLevel}");
-                textView.StatText?.SetText($"{textView.prefixStatText}: {PlayerDataModel.Instance.AttackSpeed:F2}");
-                textView.BuyText?.SetText($"{textView.prefixBuyText}: {statStore.attackSpeed.upValue}");
+                textView.LevelText?.SetText($"{textView.prefixLevelText} {PlayerDataModel.Instance.AttackSpeedLevel}");
+                textView.StatText?.SetText($"{textView.prefixStatText} {PlayerDataModel.Instance.AttackSpeed:F2}");
+                textView.BuyText?.SetText($"{textView.prefixBuyText} {statStore.attackSpeed.upValue}");
                 break;
         }
 
