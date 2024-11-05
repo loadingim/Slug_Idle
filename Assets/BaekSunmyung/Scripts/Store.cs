60ff52388aa2da600d287a0cd342fd3dda19a576
Vivid <suook15@naver.com>
2024-11-06 08:27:39 +0900

    [Feat] 상점 구매 및 강화 기능 구현
    1. 동료, 슬러그, 스킬

--- Assets/BaekSunmyung/Scripts/Store.cs
+++ Assets/BaekSunmyung/Scripts/Store.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -11,7 +13,13 @@ using UnityEngine.UI;
 
 public class Store : MonoBehaviour
 {
+    [Header("GR Ray")]
+    private GraphicRaycaster ray;
     [SerializeField] private Canvas canvas;
+    [SerializeField] private PointerEventData ped = new PointerEventData(EventSystem.current);
+    private List<RaycastResult> results = new List<RaycastResult>();
+
+
     [Header("Weapon")]
     [SerializeField] private Button heavyBtn;
     [SerializeField] private Button flameBtn;
@@ -23,7 +31,7 @@ public class Store : MonoBehaviour
     [SerializeField] private TextMeshProUGUI enhanceNum;
     [SerializeField] private Image itemIconImageInfo;
     [SerializeField] private TextMeshProUGUI curAttackTextInfo;
-    [SerializeField] private Button priceBtn; 
+    [SerializeField] private Button priceBtn;
 
     [Header("Buy Popup")]
     [SerializeField] private GameObject buyPopup;
@@ -31,13 +39,14 @@ public class Store : MonoBehaviour
     [SerializeField] private TextMeshProUGUI curAttackTextBuy;
     [SerializeField] private TextMeshProUGUI descText;
     [SerializeField] private Image itemIconImageBuy;
-    [SerializeField] private List<ShopData> shopData = new List<ShopData>(); 
+    [SerializeField] private List<ShopData> shopData = new List<ShopData>();
     private TextMeshProUGUI buyText;
 
     //WeaponInfo 자리
     private Test weaponInfoData;
     private PlayerDataModel playerDataModel;
-    
+    private GameManager gameManager;
+
     [Header("Store Button List")]
     [SerializeField] private List<Button> buttonList = new List<Button>();
     private bool[] weaponBools;
@@ -48,103 +57,89 @@ public class Store : MonoBehaviour
     private Color tureColor = new Color(1f, 1f, 1f, 1f);
     private ShopData curShopData;
     private int shopIndex = 0;
+    private bool isSelect;
+
+
 
     private void Awake()
     {
-
         //weaponInfodATA = weaponinfoData.Instace
         weaponInfoData = Test.Instance;
 
-        heavyBtn.onClick.AddListener(Heavy);
-        flameBtn.onClick.AddListener(Flame);
-        roketBtn.onClick.AddListener(Roket);
-        shotgunBtn.onClick.AddListener(ShotGun);
+        for (int i = 0; i < buttonList.Count; i++)
+        {
+            buttonList[i].onClick.AddListener(Shop);
+        }
         BuyBtn.onClick.AddListener(ItemBuy);
         priceBtn.onClick.AddListener(UpGrade);
     }
 
+    //private void OnEnable()
+    //{
+    //    GameManager.Instance.IsOpenInventory = true;
+    //}
+
+    //private void OnDisable()
+    //{
+    //    GameManager.Instance.IsOpenInventory = false;
+    //    isSelect = false;
+    //}
+
     private void Start()
-    { 
-  
+    {
+        //gameManager = GameManager.Instance;
+
+        ray = canvas.GetComponent<GraphicRaycaster>();
         for (int i = 0; i < buttonList.Count; i++)
         {
             buttonList[i].image.sprite = shopData[i].IconImage;
 
             //현재 구매한 아이템은 활성화 표시
             if (shopData[i].IsBuy)
-            { 
+            {
                 buttonList[i].GetComponent<Image>().color = new Color(1f, 1f, 1f, 1f);
-            } 
+            }
         }
-         
+
         buyText = BuyBtn.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+
+        //WeaponInfoData
         weaponInfoData = GetComponent<Test>();
+
+        // = GameObject.FindObjectWithTag("Player").GetComponentt<PlayerDataModel>();
         playerDataModel = FindObjectOfType<PlayerDataModel>();
+
+        priceBtn.interactable = false;
     }
 
     private void Update()
     {
+        ped.position = Input.mousePosition;
+        results.Clear();
+        ray.Raycast(ped, results);
+
+    }
+
+    private void Shop()
+    {
+        if (results.Count > 0)
+        {
+            string name = results[0].gameObject.name;
+            string[] strIndex = Regex.Replace(name, @"[^0-9]", " ").Split(' ');
+            shopIndex = Convert.ToInt32((strIndex[strIndex.Length - 1]));
+        }
+
         curShopData = shopData[shopIndex];
-    }
-
-    private void Heavy()
-    {
-        shopIndex = 0;
 
         if (shopData[shopIndex].IsBuy)
         {
-            ShowInfoEnhance(shopData[shopIndex]);
+            ShowInfoEnhance();
         }
         else
         {
-            ItemBuyPopup(shopData[shopIndex]);
+            ItemBuyPopup();
         }
     }
-     
-    private void Flame()
-    {
-        shopIndex = 1;
-
-        if (shopData[shopIndex].IsBuy)
-        {
-            ShowInfoEnhance(shopData[shopIndex]);
-        }
-        else
-        {
-            ItemBuyPopup(shopData[shopIndex]);
-        }
-    }
-
-    private void Roket()
-    {
-        shopIndex = 2;
-
-        if (shopData[shopIndex].IsBuy)
-        {
-            ShowInfoEnhance(shopData[shopIndex]);
-        }
-        else
-        {
-            ItemBuyPopup(shopData[shopIndex]);
-        }
-    }
-
-    private void ShotGun()
-    {
-        shopIndex = 3;
-
-
-        if (shopData[shopIndex].IsBuy)
-        {
-            ShowInfoEnhance(shopData[shopIndex]);
-        }
-        else
-        {
-            ItemBuyPopup(shopData[shopIndex]);
-        }
-    }
-
-
 
     /// <summary>
     /// 타입 별 아이템 업그레이드
@@ -159,21 +154,33 @@ public class Store : MonoBehaviour
                 Debug.Log("무기 강화 성공");
                 curShopData.CurEnhance++;
                 curShopData.IncAttack++;
-                curShopData.EnhancePrice += 20;  
+                curShopData.EnhancePrice += 20;
                 //PlayerDataModel.Attack = curShopData.Inattack;
 
                 break;
 
             case StoreType.Partner:
-
+                Debug.Log("파트너 강화 성공");
+                curShopData.CurEnhance++;
+                curShopData.IncAttack++;
+                curShopData.EnhancePrice += 20;
+                //PlayerDataModel.Attack = curShopData.Inattack;
                 break;
 
             case StoreType.Slug:
-
+                Debug.Log("슬러그 강화 성공");
+                curShopData.CurEnhance++;
+                curShopData.IncAttack++;
+                curShopData.EnhancePrice += 20;
+                //PlayerDataModel.Attack = curShopData.Inattack;
                 break;
 
             case StoreType.Skill:
-
+                Debug.Log("스킬 강화 성공");
+                curShopData.CurEnhance++;
+                curShopData.IncAttack++;
+                curShopData.EnhancePrice += 20;
+                //PlayerDataModel.Attack = curShopData.Inattack;
                 break;
         }
 
@@ -185,42 +192,76 @@ public class Store : MonoBehaviour
     /// </summary>
     private void InfoUpdate()
     {
+
         enhanceNum.text = "+" + curShopData.CurEnhance.ToString() + " " + curShopData.ItenName;
         curAttackTextInfo.text = curShopData.IncAttack.ToString();
         TextMeshProUGUI priceText = priceBtn.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
         priceText.text = curShopData.EnhancePrice.ToString();
+
+        if (10 < curShopData.EnhancePrice)
+        {
+            priceBtn.interactable = false;
+        }
+        else
+        {
+            priceBtn.interactable = true;
+        }
+
+
     }
 
     /// <summary>
     /// 선택한 아이템 강화 정보 
     /// </summary>
-    /// <param name="shopData"></param>
-    private void ShowInfoEnhance(ShopData shopData)
+    private void ShowInfoEnhance()
     {
-        curShopData = shopData; 
-        enhanceNum.text = "+" + shopData.CurEnhance.ToString() + " " + shopData.ItenName;
-        itemIconImageInfo.sprite = shopData.IconImage;
+        isSelect = !isSelect ? true : false; 
+        if (isSelect)
+        {
+            enhanceNum.text = "+" + curShopData.CurEnhance.ToString() + " " + curShopData.ItenName;
+            itemIconImageInfo.sprite = curShopData.IconImage;
 
-        //PlayerModel.Attack Change
-        curAttackTextInfo.text = shopData.IncAttack.ToString();
-        TextMeshProUGUI priceText = priceBtn.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
-        priceText.text = shopData.EnhancePrice.ToString();
+            //PlayerModel.Attack Change
+            curAttackTextInfo.text = curShopData.IncAttack.ToString();
+            TextMeshProUGUI priceText = priceBtn.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+            priceText.text = curShopData.EnhancePrice.ToString();
+
+            if (10 > curShopData.EnhancePrice)
+            {
+                priceBtn.interactable = true;
+            }
+        }
+        else
+        {
+            enhanceNum.text = "";
+            itemIconImageInfo.sprite = null;
+            curAttackTextInfo.text = "";
+            TextMeshProUGUI priceText = priceBtn.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+            priceText.text = "Price";
+            
+        }
+
+        //선택하지 않은 상태 or 보유 머니보다 아이템이 비싸면 상호작용 불가
+        if (!isSelect || 10 < curShopData.EnhancePrice)
+        {
+            priceBtn.interactable = false;
+        }
+        
     }
 
 
     /// <summary>
     /// 아이템 구매 팝업 정보
     /// </summary>
-    /// <param name="shopData"></param>
-    private void ItemBuyPopup(ShopData shopData)
+    private void ItemBuyPopup()
     {
-         
+
         buyPopup.SetActive(true);
-        itemName = shopData.ItenName;
-        curAttackTextBuy.text = shopData.IncAttack.ToString();
-        descText.text = shopData.Desc;
-        itemIconImageBuy.sprite = shopData.IconImage;
-        itemPrice = shopData.Pirce;
+        itemName = curShopData.ItenName;
+        curAttackTextBuy.text = curShopData.IncAttack.ToString();
+        descText.text = curShopData.Desc;
+        itemIconImageBuy.sprite = curShopData.IconImage;
+        itemPrice = curShopData.Pirce;
         buyText.text = itemPrice.ToString() + " Buy";
 
         //PlayerDataModel.Money < itemPrice
@@ -241,35 +282,11 @@ public class Store : MonoBehaviour
     /// </summary>
     private void ItemBuy()
     {
+        curShopData.IsBuy = true;
+        buttonList[shopIndex].GetComponent<Image>().color = new Color(1f, 1f, 1f, 1f);
         //playerDataModel.Money -= itemPrice;
- 
-        //구매한 아이템 확인 후 아이콘 활성화 표시
-        //아이템 구매 상태 전환
-        switch (itemName)
-        {
-            case "Heavy":
-                curShopData.IsBuy = true;
-                buttonList[0].GetComponent<Image>().color = new Color(1f, 1f, 1f, 1f);
-                break;
-
-            case "Flame":
-                curShopData.IsBuy = true;
-                buttonList[1].GetComponent<Image>().color = new Color(1f, 1f, 1f, 1f);
-                break;
-
-            case "Roket":
-                curShopData.IsBuy = true;
-                buttonList[2].GetComponent<Image>().color = new Color(1f, 1f, 1f, 1f);
-                break;
-
-            case "ShotGun":
-                curShopData.IsBuy = true;
-                buttonList[3].GetComponent<Image>().color = new Color(1f, 1f, 1f, 1f);
-                break; 
-        }
-
         buyPopup.SetActive(false);
-         
+
     }
 
 
